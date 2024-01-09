3b48f457b6d57f07b35e5c5bb33cea73843767d2
Leo <leodelmas31@gmail.com>
2024-01-09 16:40:39 +0100

    formation

--- src/app/formation/formation.component.ts
+++ src/app/formation/formation.component.ts
@@ -1,17 +1,18 @@
 import { Component, AfterViewInit, ElementRef, ViewChild,ViewChildren, QueryList, Renderer2 } from '@angular/core';
 import { ScriptService } from '../script.service';
-import { NgFor } from '@angular/common';
+import { NgFor,NgIf } from '@angular/common';
 
 interface DivItem {
   title: string;
   content: string;
-  isOpen: boolean;
+  link?:string;
+  link2?:string;
 }
 
 @Component({
   selector: 'app-formation',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,NgIf],
   templateUrl: './formation.component.html',
   styleUrl: './formation.component.css'
 })
@@ -19,15 +20,18 @@ export class FormationComponent {
 
   constructor(private scriptService: ScriptService) {}
 
-  divs: DivItem[] = []
+  divs: DivItem[] = [
+    {title:"Ma formation au pôle numérique de l'ADRAR",content:"J'ai choisi, afin de concrétiser mon projet de réorientation dans les métiers du numérique, de compléter une formation au pôle numérique de l'ADRAR, cette formation de neuf mois, cloturée par un stage de deux mois, m'a permis d'acquérir de solides bases en developpement d'applications web/web mobile. "},
+    {title:"Projet de fin de formation",content:"Le stage de fin de formation, effectué au sein d'une entreprise d'évènementiel, m'a permis de concevoir et de réaliser un site vitrine. La réalisation de ce site s'est accompagnée de la redaction d'un mémoire détaillant les différentes étapes du processus.",link:'assets/pictures/pdf_logo.png',link2:'assets/download/memoire_delmas.pdf'}
+  ]
 
   showContent: { [key: string]: boolean } = {
-    'about': false
+    'presentation': false,
+    'stage':false
   };
 
   toggleContent(section: string): void {
     this.showContent[section] = !this.showContent[section];
-    console.log(this.showContent['about']);
   }
 
   ngOnInit(): void {
